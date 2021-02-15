3747a2652b8d5e146709fc7001e0b68a60821fcd
Alex Rudro <jhabdurrob@gmail.com>
2021-02-15 23:01:31 +0600

    takaful project add

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Online Motor Insurance </title>
+	</head>
+	
+
+
+	<body>
+		<div style="width: 660px; height: 710px; background: #EFF791 ;" >
+			<div style="width: 654px; height: 250px; background: #0000FF; border-color: black; border: solid; text-align: center; text-shadow: red">
+				<br/> <font color="white" size="10px">Takaful Islami Insurance Limited </font><br/>
+				<font color="yellow" size="6px" face="Elephant"> Online service for Motor Insurance <br/></font>
+				<font color="red" size="7px"><b>Chuadanga Branch </b><br/></font><br/>
+				<font color="white" size="5px">Office: Hotel Royel Blue (2nd Floor), Thana Road, Chuadanga <br/>
+				Email: takaful.chuadanga@gmail.com, Mob: 01922501201</font>
+
+				
+			
+			</div><br/>
+
+		<!--img src="ime.jpg" height="200px" />-->
+		
+		 	<div style="width:653px; height:430px; background:#f5f2d0; border-color: black; border: solid; text-align: center;">
+				<font color="green" size="5px"><h1><b>Online Motor Insurance data has been submitted successfully.</b></h1></font><br/><br/><br/>
+				<a href="form.html"><font color="blue" size="5px"><b>Submit another motor insurance data</b></font></a>
+
+			</div>
+
+
+		</div>
+
+
+
+
+
+
+
+	</body>
+</html>
